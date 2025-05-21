b9fd59d1385b18c06ccd6eac53dc3013b96bf3a6
Amineo21 <153727231+Amineo21@users.noreply.github.com>
2025-05-21 09:38:34 +0200

    feat: :lipstick:

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/main')
+def index2():
+    return render_template('main.html')
